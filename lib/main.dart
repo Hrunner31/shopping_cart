20ca667a0b0ae28a2ed9060ae46760eb380cf78f
H Leonardo A Corredor <hleonard31@gmail.com>
2020-04-08 18:55:59 -0500

    Creación del widget homePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:shopping_car/src/pages/home_page.dart';
+
+void main() => runApp(MyApp());
+
 class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Container(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Shopping Car',
+      initialRoute: 'home',
+      routes: {
+        'home' : (BuildContext context) => HomePage()
+      }
     );
   }
 }
